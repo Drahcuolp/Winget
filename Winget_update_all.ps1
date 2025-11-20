21dc53b0c5253d6c40b454834ed777813ab0e15f
Luc Plouchard <luc.plouchard@gmail.com>
2025-11-20 17:34:23 +0100

    feature :  ajout comment winget

--- Winget_update_all.ps1
+++ Winget_update_all.ps1
@@ -1,5 +1,5 @@
 # Upgrade all user-scope packages
-winget upgrade --all --scope user 
+winget upgrade --all --scope user --accept-package-agreements --accept-source-agreements --disable-interactivity --silent --force
 
 # Upgrade all machine-scope packages (requires elevation)
-Start-Process winget -ArgumentList "upgrade --all --scope machine" -Verb RunAs
+Start-Process winget -ArgumentList "upgrade --all --scope machine --accept-package-agreements --accept-source-agreements --disable-interactivity --silent --force" -Verb RunAs
